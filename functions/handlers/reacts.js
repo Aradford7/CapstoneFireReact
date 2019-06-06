ff2b8c18d531df8b64d9075423acea7b921063df
Ashley Radford <aradford7@outlook.com>
2019-06-05 20:15:16 -0700

    add a like delete a like

--- functions/handlers/reacts.js
+++ functions/handlers/reacts.js
@@ -112,3 +112,90 @@ exports.commentOnReact = (req,res) =>{
         res.status(500).json({error: 'Oh no! Something went wrong!'})
     })
 }
+
+//likes for a React
+exports.likeReact = (req, res) => {
+    //firebase has max of 4mb per doc so better to store smaller and multiple collections
+    //more effcient to spread these properties bc like real twitter its slower if it was all in one doc
+    const likeDocument = db.collection('likes').where('userHandle', '==', req.user.username)
+        .where('reactId', '==', req.params.reactId).limit(1); //give array of return 1 doc array
+    //need to check if like doc exist if exist return msg already liked, check if the react exist cant like a react that doesnt exist
+    const reactDocument = db.doc(`/reacts/${req.params.reactId}`);
+    
+    let reactData;
+
+    reactDocument.get()
+    .then(doc => {
+        if(doc.exists){
+        reactData = doc.data();
+        reactData.reactId = doc.id;
+        return likeDocument.get();
+    }else{
+        return res.status(404).json({error: 'React not found.'});
+    }
+  })
+  .then(data => {
+      if(data.empty){
+          return db.collection('likes').add({
+              reactId: req.params.reactId,
+              userHandle: req.user.username
+
+          })
+          //next this promise to avoid even if not empty it will still go thru avoid it by nesting
+          .then(() => {
+              reactData.likeCount++
+              return reactDocument.update({likeCount: reactData.likeCount})
+          })
+          .then(() => {
+              return res.json(reactData); //just return the react
+          })
+      } else {
+          return res.status(400).json({error: 'React already liked'});
+      }
+  })
+  .catch(err => {
+      console.error(err)
+      res.status(500).json({error: err.code});
+  })
+}
+
+//unlike for a React
+
+exports.unlikeReact = (req, res) => {
+    const likeDocument = db.collection('likes').where('userHandle', '==', req.user.username)
+    .where('reactId', '==', req.params.reactId).limit(1); 
+    const reactDocument = db.doc(`/reacts/${req.params.reactId}`);
+
+    let reactData;
+
+    reactDocument
+    .get()
+    .then(doc => {
+        if(doc.exists){
+        reactData = doc.data();
+        reactData.reactId = doc.id;
+        return likeDocument.get();
+    }else{
+        return res.status(404).json({error: 'React not found.'});
+    }
+  })
+    .then(data => {
+        if(data.empty){
+            return res.status(400).json({error: 'React not liked'});
+    } else {
+        return db.doc(`/likes/${data.docs[0].id}`)
+        .delete()
+            .then(() => {
+                reactData.likeCount--;
+                return reactDocument.update({likeCount: reactData.likeCount})
+            })
+            .then(()=>{
+                res.json(reactData);
+            })
+        }
+    })
+    .catch(err => {
+        console.error(err)
+        res.status(500).json({error: err.code});
+    })
+}
--- functions/index.js
+++ functions/index.js
@@ -31,8 +31,8 @@ app.get('/react/:reactId', getReact);
 //POST comment on react
 app.post('/react/:reactId/comment', FBAuth, commentOnReact);
 //GET route for likes/unlikes
-// app.get('/react/:reactId/like', FBAuth, likeReact);
-// app.get('/react/:reactId/unlike', FBAuth, unlikeReact);
+app.get('/react/:reactId/like', FBAuth, likeReact);
+app.get('/react/:reactId/unlike', FBAuth, unlikeReact);
 //TODO: delete react, like a react, unlike a react, 
 
 
